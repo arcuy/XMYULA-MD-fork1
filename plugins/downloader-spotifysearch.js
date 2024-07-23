cd3182c77fb6b5b89b32f000742d46cd42ab7b83
X <159207241+XM4ZE@users.noreply.github.com>
2024-07-23 09:30:07 +0700

    Update downloader-spotifysearch.js

--- plugins/downloader-spotifysearch.js
+++ plugins/downloader-spotifysearch.js
@@ -1,15 +1,10 @@
-import { spotify } from 'betabotz-tools';
+import api from 'api-dylux';
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 	if (!text) throw `Example: ${usedPrefix + command} Melukis Senja`
-    let res = await spotify(text)
-    conn.sendMessage(m.chat, {
-		react: {
-			text: '⏳',
-			key: m.key,
-		}
-	})
-	let get_result = res.result.data
+    let res = await api.spotifySearch(text)
+    m.reply(wait)
+	let get_result = res.result
 	let ini_txt = `Found : *${text}*`
 	for (var x of get_result) {
 		ini_txt += `\n\n*Title : ${x.title}*\n`
@@ -21,7 +16,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 	m.reply(ini_txt)
 }
 
-handler.help = ['spotsearch'].map(v => v + ' <query>')
+handler.help = ['spotifysearch'].map(v => v + ' <query>')
 handler.tags = ['downloader', 'tools']
 handler.command = /^spot(ify)?search$/i
 handler.premium = false
