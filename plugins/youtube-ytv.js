5477764bfb539a4f9b305b5f0496a877424cc6dc
X <159207241+XM4ZE@users.noreply.github.com>
2024-08-03 17:17:21 +0700

    Update youtube-ytv.js

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -4,11 +4,11 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	if (!(args[0] || '').match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))) return m.reply(`Example: *${usedPrefix}${command}* https://www.youtube.com/watch?v=K9_VFxzCuQ0`)
 	await m.reply(wait);
 	try {
-		let anu = await( await ytdl(`https://api.betabotz.eu.org/api/download/ytmp4?url=${args[0]}&apikey=${lann}`)).json();
-		let txt = `*${anu.title}*\n\n`
+		let anu = await( await fetch(`https://api.betabotz.eu.org/api/download/ytmp3?url=${args[0]}&apikey=${lann}`)).json()
+		let txt = `*${anu.result.title}*\n\n`
 		+ `⌲ Resolution : 360p\n`
-		+ `⌲ Duration : ${anu.duration}`
-		await conn.sendFile(m.chat, anu.mp4, `${anu.title}.mp4`, txt, m)
+		+ `⌲ Duration : ${anu.result.duration}`
+		await conn.sendFile(m.chat, anu.mp4, `${anu.result.title}.mp4`, txt, m)
     } catch (e) {
 		console.log(e)
 	    throw global.eror
@@ -19,7 +19,7 @@ handler.menudownload = ['ytvideo <url>']
 handler.tagsdownload = ['downloader']
 handler.command = /^(yt(v(ideo)?|mp4))$/i
 
-handler.premium = false
-handler.limit = true
+handler.premium = false;
+handler.limit = true;
 
-export default handler
+export default handler;
