b0fc1eadee39c343652ce126ca1e26ee8bfc99f6
X <159207241+XM4ZE@users.noreply.github.com>
2024-08-03 17:16:08 +0700

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -1,5 +1,5 @@
 import search from 'yt-search';
-import api from 'btch-downloader';
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn, text }) => {
   if (!text) return m.reply('*example*: .play eula song');
@@ -7,7 +7,7 @@ let handler = async (m, { conn, text }) => {
     let results = await search(text);
     let videoId = results.videos[0].videoId;
     let durasi = results.videos[0].timestamp;
-    let thumbnailUrl = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
+    let thumbnailUrl = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
     let upload = results.videos[0].ago;
     let views = results.videos[0].views;
     let links = results.videos[0].url;
@@ -19,8 +19,8 @@ let handler = async (m, { conn, text }) => {
   ◦ *Upload*: ${upload}
   ◦ *Views*: ${views}
   
-YTdl By https://www.npmjs.com/package/btch-downloader
-Search By https://github.com/talmobi/yt-search
+YTdl By api.betabotz.eu.org
+Search By github.com/talmobi/yt-search
 Sent By ${global.info.namebot}`;
 
     var pesan = conn.sendMessage(m.chat, {
@@ -35,7 +35,7 @@ Sent By ${global.info.namebot}`;
                    },
                    externalAdReply: {
                    title: "AUDIO SEDANG DI KIRIM",
-                   body: `Youtube Play by Assisten ${global.info.namebot}`,
+                   body: `Youtube Play by ${global.info.namebot}`,
                    thumbnailUrl: thumbnailUrl,
                    sourceUrl: links,
                    mediaType: 1,
@@ -45,9 +45,8 @@ Sent By ${global.info.namebot}`;
             }, {});
 
     if (detik > 900) return m.reply(`Audio *${durasi}*\n_Tidak dapat mengirim, maksimal durasi 15 Menit`);
-    const yt = await api.youtube(links)
-    const link = yt.mp3
-    conn.sendMessage(m.chat, { audio: { url: link }, mimetype: 'audio/mpeg' }, { quoted: m });
+    const yt = await( await fetch(`https://api.betabotz.eu.org/api/download/ytmp3?url=${links}&apikey=${lann}`)).json()
+    await conn.sendMessage(m.chat, { audio: { url: yt.result.mp4 }, mimetype: 'audio/mpeg' }, { quoted: m });
 
   } catch (e) {
     console.log(e);
