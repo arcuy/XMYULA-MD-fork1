752ce23dbb4ff4453fdb3e2a38494abe3bd9f409
X <159207241+XM4ZE@users.noreply.github.com>
2024-08-13 13:17:48 +0700

    Update youtube-ytv.js

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import api from 'btch-downloader';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 	if (!(args[0] || '').match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))) return m.reply(`Example: *${usedPrefix}${command}* https://www.youtube.com/watch?v=K9_VFxzCuQ0`)
@@ -11,7 +12,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 		await conn.sendFile(m.chat, anu.mp4, `${anu.result.title}.mp4`, txt, m)
     } catch (e) {
 		console.log(e)
-	    throw global.eror
+        try {
+	        let btch = await api.youtube(args[0]);
+	        await conn.sendMessage(m.chat, { video: { url: btch.mp4 }, caption: wm }, { quoted: m })
+	    } catch (e) {
+	        console.log(e)
+	        m.reply(eror)
+	    }	        
     }
 };
 
