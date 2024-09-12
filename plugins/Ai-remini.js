c38dae81e6bcb028b472b635dd970cdfe761e859
X <159207241+XM4ZE@users.noreply.github.com>
2024-09-12 12:47:24 +0700

    Update Ai-remini.js

--- plugins/Ai-remini.js
+++ plugins/Ai-remini.js
@@ -1,30 +1,72 @@
-import uploadImage from '../lib/uploadImage.js';
+import uploadFile from '../lib/uploadFile.js';
 import fetch from 'node-fetch';
 
-async function handler(m, { conn, usedPrefix, command }) {
-  try {
-    const q = m.quoted ? m.quoted : m;
-    const mime = (q.msg || q).mimetype || q.mediaType || '';
-    if (/^image/.test(mime) && !/webp/.test(mime)) {
-      const img = await q.download();
-      const out = await uploadImage(img);
-      const api = await fetch(`https://api.betabotz.eu.org/api/tools/remini?url=${out}&apikey=${lann}`);
-      const image = await api.json();
-      const { url } = image 
-       conn.sendFile(m.chat, url, null, wm, m);
-    } else {
-      m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau tag gambar yang sudah dikirim.`);
-    }
-  } catch (e) {
-    console.error(e);
-    m.reply(`Identifikasi gagal. Silakan coba lagi.`);
-  }
+let handler = async (m, {
+	conn,
+	usedPrefix,
+	command
+}) => {
+	const q = m.quoted ? m.quoted : m;
+	const mime = (q.msg || q).mimetype || q.mediaType || '';
+	if (/^image/.test(mime) && !/webp/.test(mime)) {
+		const img = await q.download();
+		const out = await uploadFile(img);
+		m.reply(wait);
+		try {
+			const api = await fetch(`https://api.botcahx.eu.org/api/tools/remini?url=${out}&apikey=${btc}`);
+			const image = await api.json();
+			const {
+				url
+			} = image;
+			conn.sendFile(m.chat, url, null, wm, m);
+		} catch (e) {
+			console.log(e)
+			try {
+				const api = await fetch(`https://api.betabotz.eu.org/api/tools/remini?url=${out}&apikey=${global.lann}`);
+				const image = await api.json();
+				const {
+					url
+				} = image;
+				await conn.sendFile(m.chat, url, null, wm, m);
+			} catch (e) {
+				console.log(e)
+				try {
+					const api = await fetch(`https://api.botcahx.eu.org/api/tools/remini-v3?url=${out}&resolusi=4&apikey=${btc}`);
+					const image = await api.json();
+					const url = image.url;
+					conn.sendFile(m.chat, url, null, wm, m);
+				} catch (e) {
+					console.log(e)
+					try {
+						const api = await fetch(`https://api.betabotz.eu.org/api/tools/remini-v2?url=${out}&apikey=${lann}`);
+						const response = await api.text();
+						let image;
+						try {
+							image = JSON.parse(response);
+						} catch (error) {
+							console.error(`parse: ${error}`);
+							return;
+						}
+						const {
+							url
+						} = image;
+						conn.sendFile(m.chat, url, null, wm, m);
+					} catch (e) {
+						console.log(e);
+						m.reply(eror)
+					}
+				}
+			}
+		}
+	} else {
+		m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau reply gambar yang sudah dikirim.`);
+	}
 }
 
-handler.help = ['remini'];
-handler.tags = ['tools'];
-handler.command = ['remini'];
-handler.premium = false;
-handler.limit = false;
+handler.help = ['hd', 'remini', 'hdr']
+handler.command = ["remini", "hdr", "hd"];
+handler.tags = ['ai'];
+handler.register = true;
+handler.limit = true;
 
-export default handler;
+export default handler
