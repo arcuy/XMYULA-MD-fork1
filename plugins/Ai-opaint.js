11e587bb67e3c2f7bfcff964c977cf55d4bfaeab
X <159207241+XM4ZE@users.noreply.github.com>
2024-09-16 21:34:53 +0700

    Update Ai-opaint.js

--- plugins/Ai-opaint.js
+++ plugins/Ai-opaint.js
@@ -1,57 +1,68 @@
 import axios from 'axios';
-import uploadImage from '../lib/uploadImage.js';
+import uploadFile from '../lib/uploadFile.js';
 import fetch from 'node-fetch';
 
-const handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `
-*example:* ${usedPrefix}${command} currently in the room, Japanese themed 
-`
-  if (!text.includes(',')) throw `Tolong gunakan prompt dengan benar. Gunakan koma *[ , ]* untuk memisahkan argumen.\n*Contoh:* ${usedPrefix}${command} currently in the room, Japanese themed`;  
-  try {
-  let q = m.quoted ? m.quoted : m;
-  let media = await q.download();
-  let url = await uploadImage(media);
+const handler = async (m, {
+	conn,
+	text,
+	usedPrefix,
+	command
+}) => {
+	let q = m.quoted ? m.quoted : m;
+	let mime = (q.msg || q).mimetype || q.mediaType || '';
+	if (/^image/.test(mime) && !/webp/.test(mime)) {
+		let media = await q.download();
+		let url = await uploadFile(media);
 
-  const payload = {
-  prompt: text,
-  type: "url|base64",
-  init_image: url
-};
-  m.reply(wait)
-  const { data } = await axios.post("https://api.itsrose.rest/image/outpainting", payload, {
-    headers: { Authorization: `${global.rose}` }
-  }).catch((e) => e?.response);
+		const payload = {
+			expand_ratio: text ? text : '200',
+			init_image: url
+		};
+		try {
+			const {
+				data
+			} = await axios.post("https://api.itsrose.rest/image/outpainting", payload, {
+				headers: {
+					Authorization: `${global.rose}`
+				}
+			}).catch((e) => e?.response);
 
-  const { result, status } = data;
+			const {
+				status,
+				result
+			} = data;
 
-  if (!status) {
-    console.log(message); 
-  } else {
-    const { type, mimetype, images } = result;
-    let tipe = result.type
-    let mimitype = result.mimetype
-    
-    let medata = `*Prompt*: ${text}
-*Type*: ${tipe}
-*Mimetype*: ${mimitype}
+			if (!status) {
+				console.log(status);
+				m.reply('Hanya tersedia: [ 110, 125, 150, 200, 300 ]')
+			} else {
+				const {
+					percent,
+					image
+				} = result;
 
-${global.wm}`;
-    
-    for (const image of images) {
-      await conn.sendFile(m.chat, Buffer.from(image, 'base64'), 'gambar.jpg', medata, m)
-            }
-        }
-    } catch (e) {
-		console.log(e)
-		m.reply(eror)
-   }
+				let medata = `*Percent*: ${percent}`;
+				await conn.sendMessage(m.chat, {
+					image: {
+						url: image
+					},
+					caption: medata
+				}, {
+					mention: m
+				});
+			}
+		} catch (e) {
+			console.log(e)
+			m.reply(eror)
+		}
+	} else {
+		m.reply(`Kirim gambar dengan caption *${usedPrefix + command}* atau reply gambar yang sudah dikirim.`);
+	}
 };
 
 handler.help = ['outpainting <image & prompt>'];
 handler.tags = ['ai'];
-handler.command =  /outpainting|opaint/i
-handler.limit = true;
-handler.register = false;
-handler.premium = false;
+handler.command = /^(out(paint|painting))$/i
+handler.premium = true;
 
 export default handler
